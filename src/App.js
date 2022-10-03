d5d4267b83edec71aba0a45af86784ddbdf908dd
aadarsh805 <aadarshthakur80501@gmail.com>
2022-10-03 18:20:46 +0530

    state immutibility

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import UseState from "./components/UseState/UseState";
 import UseReducer from "./components/UseReducer/UseReducer";
+import ObjectUseState from "./components/Immutable state/ObjectUseState";
+import ArrayUseState from "./components/Immutable state/ArrayUseState";
 
 function App() {
   return (
     <div className="App">
-      <UseReducer />
+      <ObjectUseState />
+      <ArrayUseState />
     </div>
   );
 }
